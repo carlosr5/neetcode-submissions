39502c79aefc93e3debd97c101c144600d27f84f
Carlos Ramirez <cramirezpersonal@gmail.com>
2026-09-06 15:37:14 -0700

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,54 @@
+class Solution {
+private:
+    int rows;
+    int cols;
+    vector<pair<int,int>> dirs = {{0,1}, {0,-1}, {-1,0}, {1,0}};
+
+public:
+    // Iterative BFS search
+    void gridSearch(vector<vector<char>>& grid, int row, int col)
+    {
+        queue<pair<int, int>> islandCells;
+        islandCells.push({row, col});
+
+        while (!islandCells.empty())
+        {
+            pair<int,int> currCell = islandCells.front();
+            islandCells.pop();
+            
+            for (pair<int,int> dir: dirs)
+            {
+                int newRow = currCell.first + dir.first;
+                int newCol = currCell.second + dir.second;
+
+                if (0 <= newRow && newRow < rows &&
+                    0 <= newCol && newCol < cols &&
+                    grid[newRow][newCol] == '1')
+                {
+                    grid[newRow][newCol] = '0';
+                    islandCells.push({newRow, newCol});
+                }
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
+        int numIslands = 0;
+
+        for (int row = 0; row < rows; ++row)
+        {
+            for (int col = 0; col < cols; ++col)
+            {
+                if (grid[row][col] == '1')
+                {
+                    ++numIslands;
+                    gridSearch(grid, row, col);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+};
